0e6d0c4f4a17cd083efc60e1ab708860ecd57ed0
XXDOD456 <kylemcconnell94@gmail.com>
2020-05-21 12:51:35 -0500

    fixed

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { var bank = new BankAccount();
+            Console.WriteLine($"Hello, how much would you like to deposit? Your current balance is {bank}");
+            var amountToDeposit = double.Parse(Console.ReadLine());
+            bank.Deposit(amountToDeposit);
+            Console.WriteLine($"Your current balance is {bank.GetBalance()}");
+
+            var info = new PersonalInformation();
+            string number;
+            long result;
+           
+            do
+            {
+                Console.WriteLine("What is your area code and telephone number?");
+                Console.WriteLine("Using the formatting 000000000");
+
+            }
+        }
+        
+    }
+}
